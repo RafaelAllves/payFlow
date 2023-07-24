3ba9de027b2b5be1dac874834a7b0947bf03ea21
Rafael Alves <rafael.allves91@gmail.com>
2023-07-24 12:16:29 -0300

    app bar structure on homepage

---
+++ lib/modules/home/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(152),
+        child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                  text: "Olá, ",
+                  style: TextStyles.titleRegular,
+                  children: [
+                    TextSpan(
+                      text: "Rafael",
+                      style: TextStyles.titleBoldBackground,
+                    )
+                  ]
+                ),
+              ),
+              subtitle: Text("Mantenha suas contas em dia", style: TextStyles.captionShape),
+              trailing: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(5)
+                ),
+                
+              ),
+            ),
+          ),
+        ),
+        ),
+    );
+  }
+}
